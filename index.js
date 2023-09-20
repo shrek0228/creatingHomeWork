75d0b72ec9740d9267be3717d89968a2da5d5fbd
Daniil <makh.daniil@gmail.com>
2023-09-20 23:23:22 +0300

    equation

---
+++ index.js
@@ -0,0 +1,74 @@
+import { sqrt } from 'mathjs'
+import readline from 'readline'
+// const readline = require("readline")
+// const math = require("math")
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function deal(a, b, c){
+    a = Number(a)
+    b = Number(b)
+    c = Number(c)
+    if (a != 0 && b != 0 && c != 0){
+        let D = b*b - 4*a*c
+        if(D > 0){
+            let firstX = (-b + sqrt(D) / (2*a))
+            let secondX = (-b - sqrt(D) / (2*a))
+            return(firstX, secondX)
+        }
+
+        else if(D == 0){
+            let X = (-b) / (2*a)
+            return(X)
+        }
+
+        else if(D < 0){
+            return('there is no roots')
+        }
+    }
+
+    else{
+        if(a == 0){
+            let X = -(c/b)
+            return(X)
+        }
+        else if(b == 0){
+            let firstX = -(sqrt(-c/a))
+            let secondX = (sqrt(-c/a))
+            return(firstX, secondX)
+        }
+        else if(c == 0){
+            let firstX = 0
+            let secondX = -(b/a)
+        }
+        else if(a, b, c == 0){
+            return('x - R')
+        }
+        else if(b,c == 0){
+            return(0)
+        }
+        else if(a,b == 0){
+            return('there is no roots')
+        }
+        else if(a,c == 0){
+            return(0)
+        }
+    }
+}
+
+function equation(){
+    rl.question('ax^2 + bx + c = 0, choose a', (a)=>{
+        rl.question('ax^2 + bx + c = 0, choose b', (b)=>{
+            rl.question('ax^2 + bx + c = 0, choose c', (c)=>{
+                let result = deal(a, b, c)
+                console.log(result)
+            })
+        })
+    })
+}
+
+equation()
